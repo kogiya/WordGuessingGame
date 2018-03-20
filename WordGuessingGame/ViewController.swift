408c1d82d1786364005770f76c9c31a662f82ffe
Guest User <Guest@thiagos-macbook-pro.local>
2018-03-19 23:32:11 -0400

    Initial Commit

---
+++ WordGuessingGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WordGuessingGame
+//
+//  Created by Guest User on 2018-03-19.
+//  Copyright © 2018 COMP3097GBC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
