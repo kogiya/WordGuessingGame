f397cc0c5089b960868c23bbce488829cf8cb7ed
Guest User <Guest@Thiagos-MacBook-Pro.local>
2018-03-21 02:27:22 -0400

    First commit - createstoryboard

--- WordGuessingGame/ViewController.swift
+++ WordGuessingGame/ViewController.swift
@@ -20,6 +20,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    @IBAction func StartGame(_ sender: Any) {
+        print("Start button is pressed")
+    }
+    @IBAction func ShowAbout(_ sender: Any) {
+        print("Avout button is pressed")
+    }
 }
 
